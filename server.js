9ce43eddd32631b2af4d2f165bf20628d47828b4
Noam <noamg@cox.net>
2016-04-06 21:00:04 -0400

    Krav Maga

---
+++ server.js
@@ -0,0 +1,15 @@
+ogger = require('morgan')('dev'),
+server = express();
+
+var port = process.env.PORT || 8080;
+
+server.use(express.static(__dirname+'/public'));
+server.use(logger);
+
+server.get('/', function(req, res){
+ res.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(8080, function () {
+ console.log('Now listen on port'+ 8080);
+});
