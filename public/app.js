503495dcfd6bc2dfaf391a489ac836cd17556986
Noam <noamg@cox.net>
2016-04-08 20:52:02 -0400

    Krav Maga

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ app.controller('controller', function($scope){
         Description: 'IDF soldiers soldiers training',
       },
       img: "http://www.solveisraelsproblems.com/wp-content/uploads/2012/03/Krav-Maga-620x310.jpg",
-      background:"http://www.gkrav.com/images/Krav_Maga_Glasgow.png",
+      background:"http://www.walldevil.com/wallpapers/a45/anime-samurai-japanese-wallpaper-pictures-bleach-wallpapers.jpg",
     },
     {
       video:"C3xayIEKGf0",
@@ -15,7 +15,7 @@ app.controller('controller', function($scope){
         Description:"Krav Maga sample techniques",
       },
       img:"https://i.ytimg.com/vi/6Da8zhAWYzE/maxresdefault.jpg",
-      background:"http://movementmartialarts.com/home/wp-content/uploads/2014/01/orlando-martial-arts-karate-kicks-300x106.jpg",
+      background:"http://sf.co.ua/13/05/wallpaper-2792981.jpg",
     },
     {
       video:"8-HUxeIxFsQ",
@@ -23,7 +23,7 @@ app.controller('controller', function($scope){
         Description:"Defense against a Gun",
       },
       img:"http://karatefit.com/files/2014/11/Pic6.jpg",
-      background:"https://s-media-cache-ak0.pinimg.com/236x/2d/2a/c7/2d2ac79cc0eb6d052212cbc2ddeb74ae.jpg",
+      background:"http://i.fonts2u.com/ka/karate-chop_4.png",
     },
     {
       video:"UFnmq5PPScA",
@@ -31,7 +31,7 @@ app.controller('controller', function($scope){
         Description:"The Bourne Identity fighting style",
       },
       img:"http://dvdmedia.ign.com/dvd/image/bourne_06.jpg",
-      background: "http://www.counter-racism.com/analysis/moviereviews/bourne/header.jpg",
+      background: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSqIQeX0kuiYWTUfI63r-RCHrtKP3thkSb3ySwZYGaQ2lS4MuKhBQ",
     },
   ];
 });
